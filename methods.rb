f3d279f3b5d67e692df8268eb3935b6f8ef60868
github username <github email address>
2019-07-10 09:39:16 -0400

    Done.

---
+++ methods.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+numbs = [0,12,3,4,5,0,10]
+
+def move_zeros(nums)
+    arr = []
+    nums.each do |num| 
+        if num == 0 
+            arr << num 
+        elsif 
+            num == 0
+             numbs.destory(num)
+        end 
+            b = arr.length 
+            b.times do 
+                nums.push(0)
+                binding.pry
+            end
+        end
+end
+move_zeros(numbs)
